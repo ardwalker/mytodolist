dfa58f07f4e20e4296930eb76adebc3cee7a906c
Andrew Walker <andrew.walker1@ihg.com>
2016-01-23 12:57:43 -0500

    Added project files

---
+++ app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+])
+
+.controller('TodoListController', ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+  $scope.todos = ['Do this', 'Do that'];
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+
+
