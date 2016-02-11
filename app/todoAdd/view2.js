1123945cc4b25f64be231de735c16b624cdd9f7c
Andrew Walker <andrew.walker1@ihg.com>
2016-02-10 22:04:23 -0500

    updates

--- app/todoAdd/view2.js
+++ app/todoAdd/view2.js
@@ -1,14 +1,26 @@
 'use strict';
 
-angular.module('myApp.todoAdd', ['ngRoute'])
+var appTodoAdd = angular.module('myApp.todoAdd', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+appTodoAdd.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/add', {
     templateUrl: 'todoAdd/view2.html',
     controller: 'View2Ctrl'
   });
-}])
+}]);
 
-.controller('View2Ctrl', [function() {
+appTodoAdd.controller('View2Ctrl', ['$scope', function($scope) {
+
+  $scope.master = {};
+
+  $scope.update = function(todo) {
+    $scope.master = angular.copy(todo);
+  };
+
+  $scope.reset = function() {
+    $scope.todo = angular.copy($scope.master);
+  };
+
+  $scope.reset();
 
 }]);
--- app/todoList/todoList.js
+++ app/todoList/todoList.js
@@ -1,17 +1,17 @@
 'use strict';
 
-angular.module('myApp.todoList', ['ngRoute'])
+var appTodoList = angular.module('myApp.todoList', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+appTodoList.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/list', {
-    templateUrl: 'todoList/view1.html',
-    controller: 'View1Ctrl'
+    templateUrl: 'todoList/todoList.html',
+    controller: 'TodoListCtrl'
   });
-}])
+}]);
 
-.service('todoListService',["$http","$log", function($http,$log){
-
+appTodoList.service('todoListService',["$http","$log", function($http,$log){
   var self = this;
+
   this.items = [
     { item: 1, name: "todo 1", description: "This is todo item 1 description" },
     { item: 2, name: "todo 2", description: "This is todo item 2 description" }
@@ -20,9 +20,9 @@ angular.module('myApp.todoList', ['ngRoute'])
   this.GetTodoList = function() {
     return self.items;
   }
-}])
+}]);
 
-.controller('View1Ctrl', ["$scope","$log","todoListService", function($scope,$log,todoListService) {
+appTodoList.controller('TodoListCtrl', ["$scope","$log","todoListService", function($scope,$log,todoListService) {
 
   var self = this;
 
